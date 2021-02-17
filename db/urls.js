7fe7d1eb1c0044aef8cc14fd71b49b35d99b0528
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-17 23:50:08 +0000

    Name new methods to implement

--- db/urls.js
+++ db/urls.js
@@ -1,4 +1,9 @@
+const generateRandomString = require("../helper/generate");
+
 const urlDatabase = {
+  addLink: function(longURL) {},
+  deleteLink: function(linkID) {},
+  modifyLink: function(linkID, longURL) {},
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" },
 };
