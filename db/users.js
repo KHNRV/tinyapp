3899d0c4717a4da171326285a8bde353b3aa41e9
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-17 21:12:20 +0000

    added registration method

--- db/users.js
+++ db/users.js
@@ -1,4 +1,21 @@
+const { generateRandomString } = require("../helper/generate");
+
 const users = {
+  register: function(user) {
+    // Define newID
+    let newID;
+    // Generate a ID that does not exist in the db
+    do {
+      newID = generateRandomString();
+    } while (!Object.keys(this).includes(newID));
+    // Get the user details
+    const { email, password } = user;
+    // Register the user in the db under newID
+    this[newID] = { id: newID, email, password };
+
+    // Return new user
+    return this[newID];
+  },
   "kevin.nicolas": {
     id: "kevin.nicolas",
     email: "kevin.nicolas@hey.com",
