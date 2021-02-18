c8d2fba8dc55fc210fa6c8ddf964432a1d9e7b0a
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-18 01:24:57 +0000

    BUG FIX: Deisplay long URL proprely

--- express_server.js
+++ express_server.js
@@ -107,7 +107,7 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
+    longURL: urlDatabase[req.params.shortURL].longURL,
     user: users[req.cookies["user_id"]],
   };
   res.render("urls_show", templateVars);
