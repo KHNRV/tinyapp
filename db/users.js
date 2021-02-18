b3494942b6b25e4b534c81613851634692de0e0a
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-18 14:46:48 +0000

    create UserDB class, make users an instance of it

--- db/users.js
+++ db/users.js
@@ -4,8 +4,25 @@ const {
 } = require("../helper/generate");
 const bcrypt = require("bcrypt");
 
-const users = {
-  register: function(user) {
+class UserDB {
+  constructor() {
+    this["kevin.nicolas"] = {
+      id: "kevin.nicolas",
+      email: "kevin.nicolas@hey.com",
+      password: "$2b$10$Q1/q8EYpM2Pc/iWoFHhIZeYGN3tEo0jjMoI5DmC/vV/VRtI2CG9iC", // "qwerty"
+    };
+    this.userRandomID = {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "$2b$10$9cK.XUp22emckDHB7e.TOOR6pLYX2/UtqUhhUmykgG87NRTTrBrcK", // "purple-monkey-dinosaur"
+    };
+    this.user2RandomID = {
+      id: "user2RandomID",
+      email: "user2@example.com",
+      password: "$2b$10$OAvD6SNjojM4xFEZqsnC3uwrqgzPMY9WnmfusSaFcnrCHgPXLN196", // "dishwasher-funk"
+    };
+  }
+  register(user) {
     // Is entry valid ?
     if (!user.email || !user.password) {
       return null;
@@ -29,8 +46,8 @@ const users = {
     this[newID] = { id: newID, email, password };
     // Return new user
     return this[newID];
-  },
-  loginCheck: function(credentials) {
+  }
+  loginCheck(credentials) {
     // Test if the email exists
     const userID = isEmailDuplicate(credentials.email, this);
     if (userID) {
@@ -42,22 +59,9 @@ const users = {
     }
     // Return "" otherwise
     return "";
-  },
-  "kevin.nicolas": {
-    id: "kevin.nicolas",
-    email: "kevin.nicolas@hey.com",
-    password: "$2b$10$Q1/q8EYpM2Pc/iWoFHhIZeYGN3tEo0jjMoI5DmC/vV/VRtI2CG9iC", // "qwerty"
-  },
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "$2b$10$9cK.XUp22emckDHB7e.TOOR6pLYX2/UtqUhhUmykgG87NRTTrBrcK", // "purple-monkey-dinosaur"
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "$2b$10$OAvD6SNjojM4xFEZqsnC3uwrqgzPMY9WnmfusSaFcnrCHgPXLN196", // "dishwasher-funk"
-  },
-};
+  }
+}
+
+const users = new UserDB();
 
 module.exports = { users };
