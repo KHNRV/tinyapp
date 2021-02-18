6a1697620df41974dbe661165f2e0f56b40be1f8
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-18 17:40:12 +0000

    UserDB now accept baseDB when constructed

--- db/users.js
+++ db/users.js
@@ -1,5 +1,21 @@
 const { UserDB } = require("../helper/UserDB_helper");
 
-const users = new UserDB();
+const users = new UserDB({
+  "kevin.nicolas": {
+    id: "kevin.nicolas",
+    email: "kevin.nicolas@hey.com",
+    password: "$2b$10$Q1/q8EYpM2Pc/iWoFHhIZeYGN3tEo0jjMoI5DmC/vV/VRtI2CG9iC", // "qwerty"
+  },
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "$2b$10$9cK.XUp22emckDHB7e.TOOR6pLYX2/UtqUhhUmykgG87NRTTrBrcK", // "purple-monkey-dinosaur"
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "$2b$10$OAvD6SNjojM4xFEZqsnC3uwrqgzPMY9WnmfusSaFcnrCHgPXLN196", // "dishwasher-funk"
+  },
+});
 
 module.exports = { users };
--- helper/UserDB_helper.js
+++ helper/UserDB_helper.js
@@ -2,22 +2,11 @@ const { generateRandomString } = require("../helper/helper");
 const bcrypt = require("bcrypt");
 
 class UserDB {
-  constructor() {
-    this["kevin.nicolas"] = {
-      id: "kevin.nicolas",
-      email: "kevin.nicolas@hey.com",
-      password: "$2b$10$Q1/q8EYpM2Pc/iWoFHhIZeYGN3tEo0jjMoI5DmC/vV/VRtI2CG9iC", // "qwerty"
-    };
-    this.userRandomID = {
-      id: "userRandomID",
-      email: "user@example.com",
-      password: "$2b$10$9cK.XUp22emckDHB7e.TOOR6pLYX2/UtqUhhUmykgG87NRTTrBrcK", // "purple-monkey-dinosaur"
-    };
-    this.user2RandomID = {
-      id: "user2RandomID",
-      email: "user2@example.com",
-      password: "$2b$10$OAvD6SNjojM4xFEZqsnC3uwrqgzPMY9WnmfusSaFcnrCHgPXLN196", // "dishwasher-funk"
-    };
+  constructor(baseDB) {
+    // Add baseDB values to this instance
+    for (const user in baseDB) {
+      this[user] = baseDB[user];
+    }
   }
   register(user) {
     // Is entry valid ?
