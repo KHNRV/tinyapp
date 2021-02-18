798049804f5ee5fbbd39c274f2dab0c198d38910
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-18 17:29:43 +0000

    seperate the UrlDB class from instances

---
+++ helper/UrlDB_helper.js
@@ -0,0 +1,58 @@
+const { generateRandomString } = require("./helper");
+
+class UrlDB {
+  constructor() {
+    // Test entries
+    this.b6UTxQ = { longURL: "https://www.tsn.ca", userID: "kevin.nicolas" };
+    this.i3BoGr = { longURL: "https://www.google.ca", userID: "aJ48lW" };
+  }
+
+  addLink(longURL, userID) {
+    // Define newShortURL
+    let newShortURL = "";
+    // Generate a unique newShortURL
+    do {
+      newShortURL = generateRandomString();
+    } while (this[newShortURL]);
+    // add the redirection to the db
+    this[newShortURL] = { longURL, userID };
+    // return  newTinyURL object
+    console.log(this[newShortURL]);
+    return newShortURL;
+  }
+
+  deleteLink(linkID, userID) {
+    // If user is authorized, delete the link
+    if (this[linkID].userID === userID) {
+      delete this[linkID];
+      return true;
+    }
+    return false;
+  }
+
+  modifyLink(linkID, longURL, userID) {
+    // If user is authorized, modifyt the link
+    if (this[linkID].userID === userID) {
+      this[linkID].longURL = longURL;
+      return true;
+    }
+    return false;
+  }
+  getLongUrl(linkID) {
+    return this[linkID].longURL;
+  }
+  getUrlsForUser(userID) {
+    // Define userLinks object
+    const userLinks = {};
+
+    // Iterate in this db
+    for (const urlID in this) {
+      if (this[urlID].userID === userID) {
+        userLinks[urlID] = this[urlID];
+      }
+    }
+    return userLinks;
+  }
+}
+
+module.exports = { UrlDB };
