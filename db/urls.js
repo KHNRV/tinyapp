da52a3238f5d60d3dd15d237b633f41c5b5484f3
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-18 00:06:24 +0000

    Implement deleteLink method

--- db/urls.js
+++ db/urls.js
@@ -13,7 +13,14 @@ const urlDatabase = {
     // return  newTinyURL object
     return this[newShortURL];
   },
-  deleteLink: function(linkID, userID) {},
+  deleteLink: function(linkID, userID) {
+    // If user is authorized, delete the link
+    if (this[linkID].userID === userID) {
+      delete this[linkID];
+      return true;
+    }
+    return false;
+  },
   modifyLink: function(linkID, longURL, userID) {},
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" },
