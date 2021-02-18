4ad6bffd6b276f32a2d971cd590bcb63b42289e3
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-18 00:02:43 +0000

    Implement the addLink function

--- db/urls.js
+++ db/urls.js
@@ -1,9 +1,20 @@
 const generateRandomString = require("../helper/generate");
 
 const urlDatabase = {
-  addLink: function(longURL) {},
-  deleteLink: function(linkID) {},
-  modifyLink: function(linkID, longURL) {},
+  addLink: function(longURL, userID) {
+    // Define newShortURL
+    let newShortURL = "";
+    // Generate a unique newShortURL
+    do {
+      newShortURL = generateRandomString();
+    } while (this.newShortURL);
+    // add the redirection to the db
+    this[newShortURL] = { longURL, userID };
+    // return  newTinyURL object
+    return this[newShortURL];
+  },
+  deleteLink: function(linkID, userID) {},
+  modifyLink: function(linkID, longURL, userID) {},
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" },
 };
