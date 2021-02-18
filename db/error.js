a6dfa7ed1d7818b8dfeb5604dd0c8aa7e2c382e6
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-18 23:57:41 +0000

    create an error message db

---
+++ db/error.js
@@ -0,0 +1,35 @@
+const error = {
+  400: {
+    code: 400,
+    message: "You're such a bad request...",
+    description: "I could not understand what you asked me",
+    solutionLink: "/",
+    solutionMessage: "Start again?",
+  },
+  401: {
+    code: 401,
+    message: "Ehh... Do I know you?",
+    description:
+      "I'd be glad to give you acces to that page, but maybe tell me who you are first.",
+    solutionLink: "/login",
+    solutionMessage: "Login",
+  },
+  403: {
+    code: 403,
+    message: "WOW, stop right there!",
+    description:
+      "It looks like you have no buisiness trying to access that page",
+    solutionLink: "/",
+    solutionMessage: "Please move along",
+  },
+  404: {
+    code: 404,
+    message: "Well, that sucks...",
+    description:
+      "I cannot find what you are asking for. Although, I do have an amazing 4K 60FPS video that explains what happend!",
+    solutionLink: "https://youtu.be/2ocykBzWDiM",
+    solutionMessage: "Watch 404 Explained",
+  },
+};
+
+module.exports = { error };
